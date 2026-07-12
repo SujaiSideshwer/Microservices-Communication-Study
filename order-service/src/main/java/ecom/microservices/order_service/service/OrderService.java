66fbf9e907a8c1582df0bc8f1e048f3a8242dc85
Sujai Sideshwer <sujaisideshwer17@gmail.com>
2026-07-12 23:05:20 +0530

    only RestTemplate used

--- order-service/src/main/java/ecom/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/ecom/microservices/order_service/service/OrderService.java
@@ -1,11 +1,8 @@
 package ecom.microservices.order_service.service;
 
 import ecom.microservices.order_service.dto.Inventory;
-import ecom.microservices.order_service.exception.Custom4xxException;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -13,25 +10,19 @@ public class OrderService {
 
     private final RestTemplate restTemplate;
 
-    private final RestClient restClient;
-
-    public OrderService(RestTemplate restTemplate, RestClient restClient) {
+    public OrderService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.restClient = restClient;
     }
 
     public String placeOrder(Long productId){
+        ResponseEntity<Inventory> entity = restTemplate.getForEntity(
+                "http://localhost:8081/inventory/" + productId,
+                Inventory.class
+        );
 
-////Using RestClient - readable and manageable code
-        ResponseEntity<Inventory> entity= restClient.get()
-                .uri("http://localhost:8081/inventory/{productId}", productId)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
-                    throw new Custom4xxException(response.getStatusCode(), response.getHeaders());
-                })
-                .toEntity(Inventory.class);
+        Inventory inventory = entity.getBody();
 
-        updateInventory(entity.getBody());
+        updateInventory(inventory);
 
         return entity.getBody() != null && entity.getBody().getQuantity() > 0
                 ? "order placed successfully" : "product out of stock";
@@ -39,10 +30,6 @@ public class OrderService {
 
     private void updateInventory(Inventory inventory) {
         inventory.setQuantity(inventory.getQuantity() - 1);
-        restClient.post()
-                .uri("http://localhost:8081/inventory")
-                .body(inventory)
-                .retrieve()
-                .toBodilessEntity();
+        restTemplate.postForEntity("http://localhost:8081/inventory", inventory, String.class);
     }
 }
